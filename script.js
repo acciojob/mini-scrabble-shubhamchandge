99fff024f52b66e94a692bb00981a0562f174dd4
shubham chandge <115560962+shubhamchandge@users.noreply.github.com>
2022-12-28 15:35:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let text = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+
+text.addEventListener("input", ()=>{
+    letterCount.innerHTML=text.value.length;
+})
+
+console.log("working");
